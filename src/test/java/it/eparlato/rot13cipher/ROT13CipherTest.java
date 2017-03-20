ddf286e2f375600359092a29c69f40d7077222ee
eparlato <eparlato82@gmail.com>
2017-03-20 18:45:56 +0100

    First commit.

---
+++ src/test/java/it/eparlato/rot13cipher/ROT13CipherTest.java
@@ -0,0 +1,13 @@
+package it.eparlato.rot13cipher;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ROT13CipherTest {
+
+	@Test
+	public void testName() throws Exception {
+		assertTrue(true);
+	}
+}
