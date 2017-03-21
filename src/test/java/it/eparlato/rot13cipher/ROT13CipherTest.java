ff5e1492a6f62f416a61ecf01c5543c4da69a462
eparlato <eparlato82@gmail.com>
2017-03-21 18:41:07 +0100

    When the input is A the hard-coded output is N.

--- src/test/java/it/eparlato/rot13cipher/ROT13CipherTest.java
+++ src/test/java/it/eparlato/rot13cipher/ROT13CipherTest.java
@@ -8,30 +8,37 @@ public class ROT13CipherTest {
 
 	@Test
 	public void should_return_empty_if_input_is_null() throws Exception {
-		
 		assertEquals("", ROT13Cipher.transform(null));
 	}
-	
+
 	// "" => ""
-	
+
 	// "A" => "N"
-	
+	@Test
+	public void should_return_N_if_input_is_A() throws Exception {
+		assertEquals("N", ROT13Cipher.transform("A"));
+	}
+
 	// "F" => "S"
-	
+
 	// "Z" => "M"
-	
+
 	// "b" => "o"
-	
+
 	// "JeDi" => "WrQv"
-	
+
 	// "rkgerzr" => "extreme"
-	
+
 	// "EXTREME" => "RKGERZR"
-	
+
 	public static class ROT13Cipher {
 
 		public static String transform(String input) {
-			return "";
+			if (input == null) {
+				return "";
+			} else {
+				return "N";
+			}
 		}
 
 	}
